e1a2e29d10109f350cf7f05addc28e27078f6015
anders <anders@1f418930-7ff8-0310-b8ad-c653122473bc>
2008-02-26 23:10:39 +0000

    trying to handle silvercity egg on both platforms
    git-svn-id: http://svn.ccnmtl.columbia.edu/rollo/trunk@9696 1f418930-7ff8-0310-b8ad-c653122473bc

--- init.sh
+++ init.sh
@@ -6,10 +6,14 @@ source working-env/bin/activate
 export NODE=`uname --nodename`
 if [ $NODE = "kodos.ccnmtl.columbia.edu" ] || [ $NODE = "monty.ccnmtl.columbia.edu" ]; then
    mv eggs/psycopg2-2.0.6-py2.5-linux-x86_64.egg eggs/psycopg2-2.0.6-py2.5-linux-x86_64.tmp
+   mv eggs/SilverCity-0.9.7-py2.5-linux-x86_64.egg eggs/SilverCity-0.9.7-py2.5-linux-x86_64.egg-64
+   mv eggs/SilverCity-0.9.7-py2.5-linux-x86_64.egg-32 eggs/SilverCity-0.9.7-py2.5-linux-x86_64.egg
 fi
 easy_install -H None -f eggs eggs/*.egg
 if [ $NODE = "kodos.ccnmtl.columbia.edu" ] || [ $NODE = "monty.ccnmtl.columbia.edu" ]; then
    ln -s /usr/lib/python2.5/site-packages/mx/ working-env/lib/python2.5/
    ln -s /usr/lib/python2.5/site-packages/psycopg2/ working-env/lib/python2.5/
    mv eggs/psycopg2-2.0.6-py2.5-linux-x86_64.tmp eggs/psycopg2-2.0.6-py2.5-linux-x86_64.egg
+   mv eggs/SilverCity-0.9.7-py2.5-linux-x86_64.egg eggs/SilverCity-0.9.7-py2.5-linux-x86_64.egg-32
+   mv eggs/SilverCity-0.9.7-py2.5-linux-x86_64.egg-64 eggs/SilverCity-0.9.7-py2.5-linux-x86_64.egg
 fi
